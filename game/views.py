24efd345894054ba658a4fbff5d371efdf332f27
ZagY <2439835570@qq.com>
2022-06-26 20:29:15 +0800

    create project struct

--- game/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1 style="text-align: center">My First Website！！！！</h1>'
-    line2 = '''<img src='https://cdn.acwing.com/media/user/profile/photo/59888_lg_c9ee13c030.jpg' width='800'>'''
-    return HttpResponse(line1 + line2)
